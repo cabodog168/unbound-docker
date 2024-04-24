7c41d8272e966dd18eae08552db744fb71331b03
Ku.Chang <kuchang@gmail.com>
2024-04-24 10:29:21 -0700

    Updated build script/unbound config

--- docker_build.sh
+++ docker_build.sh
@@ -8,29 +8,15 @@ TARGET_ARMV7="linux/arm/v7"
 EXPORTER_AMD64="unbound_exporter_amd64"
 EXPORTER_ARMV7="unbound_exporter_armv7"
 
-if [ $# -lt 4 ] && [ $# -gt 5 ]; then
-    echo "Invalid number of parameters. Expected 4 or 5 arguments."
+if [ $# -ne 6 ]; then
+    echo "Invalid number of arguments. Expected 6 arguments."
     exit 1
 fi
 
-if [ $# -eq 5 ]; then
-    case "$5" in
-        "amd64")
-            # build linux/amd64 image and include amd64 version of exporter
-            target=$TARGET_AMD64
-            exporter=$EXPORTER_AMD64
-            ;;
-        "armv7")
-            # build linux/amdv7 image and include armv7 version of exporter
-            target=$TARGET_ARMV7
-            exporter=$EXPORTER_ARMV7
-            ;;
-        *)
-            # anyting else default to amd64 platform
-            target=$TARGET_AMD64
-            exporter=$EXPORTER_AMD64
-            ;;
-    esac
+if [ "$6" == "armv7" ]; then
+    # build linux/amdv7 image and include armv7 version of exporter
+    target=$TARGET_ARMV7
+    exporter=$EXPORTER_ARMV7
 else
     # No platform argument provided, default to amd64 platform
     target=$TARGET_AMD64
@@ -41,21 +27,34 @@ echo -e "${YELLOW}Unbound version: $1${NC}"
 echo -e "${YELLOW}Unbound SHA256: $2${NC}"
 echo -e "${YELLOW}Unbound docker image version: $3${NC}"
 echo -e "${YELLOW}OpenSSL build env version: $4${NC}"
-echo -e "${YELLOW}Platform: $target${NC}"
+echo -e "${YELLOW}Fresh build: $5${NC}"
+echo -e "${YELLOW}Target platform: $6${NC}"
 
 cp ./exporter/bin/$exporter ./exporter/bin/unbound_exporter
 echo -e "copied $exporter to unbound_exporter"
 
-docker build \
-  --no-cache \
-  --platform "$target" \
-  --build-arg UNBOUND_VERSION="$1" \
-  --build-arg UNBOUND_SHA256="$2" \
-  --build-arg UNBOUND_DOCKER_IMAGE_VERSION="$3" \
-  --build-arg OPENSSL_BUILDENV_VERSION="$4" \
-  -f unbound/release.Dockerfile \
-  -t cabodog/unbound:latest \
-  .
+if [ "$5" == "true" ]; then
+    docker build \
+    --no-cache \
+    --platform "$target" \
+    --build-arg UNBOUND_VERSION="$1" \
+    --build-arg UNBOUND_SHA256="$2" \
+    --build-arg UNBOUND_DOCKER_IMAGE_VERSION="$3" \
+    --build-arg OPENSSL_BUILDENV_VERSION="$4" \
+    -f unbound/release.Dockerfile \
+    -t cabodog/unbound:latest \
+    .
+else
+    docker build \
+    --platform "$target" \
+    --build-arg UNBOUND_VERSION="$1" \
+    --build-arg UNBOUND_SHA256="$2" \
+    --build-arg UNBOUND_DOCKER_IMAGE_VERSION="$3" \
+    --build-arg OPENSSL_BUILDENV_VERSION="$4" \
+    -f unbound/release.Dockerfile \
+    -t cabodog/unbound:latest \
+    .
+fi
 
 rm -f ./exporter/bin/unbound_exporter
 echo -e "deleted temporary unbound_exporter"
--- unbound/root/unbound.sh
+++ unbound/root/unbound.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+chmod +x /usr/local/unbound/unbound.d/sbin/unbound_exporter
 /usr/local/unbound/unbound.d/sbin/unbound-anchor -a /usr/local/unbound/iana.d/root.key
 /usr/local/unbound/unbound.d/sbin/unbound_exporter -unbound.ca "" -unbound.cert "" -unbound.host "unix:///run/unbound/unbound.ctl" &
 /usr/local/unbound/unbound.d/sbin/unbound -d -c /usr/local/unbound/unbound.conf &
